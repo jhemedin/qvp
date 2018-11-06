4ac0ee9a58815f12b0e451e220a5e1dadda7132a
jhemedin <jason.hemedinger@valpo.edu>
2018-11-06 11:32:22 -0600

    ADD: qvp code and unit test

--- qvp/qvp.py
+++ qvp/qvp.py
@@ -2,47 +2,49 @@
 pyart.retrieve.quasi_vertical_profile
 =====================================
 
-Retrieval of QVPs from a radar object. 
+Retrieval of QVPs from a radar object
 
 .. autosummary::
-    :
-    :
+    :toctree: generated/
 
     quasi_vertical_profile
-
 
 """
 
 import numpy as np
 
-from ..core import antenna_to_cartesian
+from ..core.transforms import antenna_to_cartesian
 
 
 
-def quasi_vertical_profile(radar, fields=None, gatefilter=None):
+def quasi_vertical_profile(radar, fields = None, gatefilter = None):
+    
     """
     Quasi Vertical Profile.
     
-    Creates a QVP object containing ....
-    can then be used to plot and produce the quasi vertical profile.
-
+    Creates a QVP object ....
+    
+    
     Parameters
     ----------
     radar : Radar
         Radar object used.
-    fields : string
-        Radar fields to use for QVP output
-
+    field : string
+        Radar field to use for QVP calculation.
+    
     Other Parameters
     ----------------
     gatefilter : GateFilter
         A GateFilter indicating radar gates that should be excluded
-        from the import qvp calculation.
-
+        from the import qvp calculation
+        
     Returns
     -------
-    qvp : 
-
+    qvp : Dictonary
+        Dictonary containing fields from radar object
+    
+    
+    
     References
     ----------
     Troemel, S., M. Kumjian, A. Ryzhkov, and C. Simmer, 2013: Backscatter 
@@ -58,27 +60,42 @@ def quasi_vertical_profile(radar, fields=None, gatefilter=None):
     radar data. Submitted to J. Atmos. Oceanic Technol.
     
     """
-
-    if fields is None:
-        fields = radar.fields
-  
-    #if gatefilter is not None:
-        #add gatefilter arguments here
-
+    
+    qvp = {}
     desired_angle = 20.0
     index = abs(radar.fixed_angle['data'] - desired_angle).argmin()
     print(radar.fixed_angle['data'])
     print(radar.elevation['data'][-1])
+    
+    radar_slice = radar.get_slice(index)
+    
+    if fields is None:
+        fields = radar.fields
 
-    qvp = {}
+        for field in fields:
+            if gatefilter is not None:
+                get_fields = radar.get_field(index, field)
+                mask_fields = np.ma.masked_where(gatefilter.gate_excluded[radar_slice], 
+                                                 get_fields)
+                radar_fields = np.ma.mean(mask_fields, axis = 0)
+            else: 
+                radar_fields = radar.get_field(index, field).mean(axis = 0)
+            qvp.update({field:radar_fields})
 
-    for field in fields:
-        this_field = radar.get_field(index, field).mean(axis=0)
-        qvp.update({field:this_field})
-
+    else:
+        if gatefilter is not None:
+            get_field = radar.get_field(index, fields)
+            mask_field = np.ma.masked_where(gatefilter.gate_excluded[radar_slice], 
+                                            get_field)
+            radar_field = np.ma.mean(mask_field, axis = 0)
+        else: 
+            radar_field = radar.get_field(index, fields).mean(axis = 0)
+            
+        qvp.update({fields:radar_field})
+        
     qvp.update({'range': radar.range['data'], 'time': radar.time})
     x,y,z = antenna_to_cartesian(qvp['range']/1000.0, 0.0, 
-                                 radar.fixed_angle['data'][index])
+                                            radar.fixed_angle['data'][index])
     qvp.update({'height': z})
-    
-    return qvp   
+    del radar
+    return qvp
